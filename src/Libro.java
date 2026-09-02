9f1f75fe2fb968bd66e558a720fa70fda61030c1
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-09-02 10:56:48 -0500

    // funciones / getters y setters de la calse libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,16 +1,52 @@
 public class Libro {
+    // atributos
     private String titulo, autor, isbn, genero;
     private int año, copiasDisponibles;
 
-
+//constructor
 public Libro(String titulo, String autor, String isbn, String genero, int año, int copiasDisponibles){
-    this.titulo=titulo;
-    this.autor=autor;
-    this.isbn=isbn;
-    this.genero=genero;
-    this.año=año;
-    this.copiasDisponibles=copiasDisponibles;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.isbn = isbn;
+    this.genero = genero;
+    this.año = año;
+    this.copiasDisponibles = copiasDisponibles;
 }
+   // funciones / getters y setters
+
+    //titulo
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    //autor
+
+    public String getAutor() {return  autor;}
+    public void setAutor(String autor) { this.autor = autor; }
+
+    //isbn
+
+    public String getIsbn() {return  isbn;}
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    //genero
+
+    public String getGenero() {return  genero;}
+    public void setGenero(String genero) { this.genero = autor; }
+
+    //año
+
+    public int getAño() {return  año;}
+    public void setAño(int año) { this.año = año; }
+
+    //copias Disponibles
+
+    public int getCopiasDisponibles() {return  copiasDisponibles;}
+    public void setCopiasDisponibles(int copiasDisponibles) { this.copiasDisponibles = copiasDisponibles; }
+
+
+
+
 
 
 }
