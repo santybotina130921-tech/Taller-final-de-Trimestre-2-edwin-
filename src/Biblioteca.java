8c121aba442f529e3bc1595fa7b5e5cfcaf2f029
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-09-02 11:12:47 -0500

    creacion de colecion y constructor  de la clase biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Biblioteca {
+    private ArrayList<Libro> listaLibros;
+    private HashMap<String, Libro> mapaIsbn;
 
-
+    // Constructor
+    public Biblioteca() {
+        this.listaLibros = new ArrayList<>();
+        this.mapaIsbn = new HashMap<>();
+    }
 }
