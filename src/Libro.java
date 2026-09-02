36d95835bbe5fe64eec0d69548f7aef8bf4e55c0
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-09-02 10:38:43 -0500

    atributos y constructor de la calse libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,7 +1,16 @@
 public class Libro {
-    public static void main(String[] args) {
-        System.out.println("hola");
-    }
+    private String titulo, autor, isbn, genero;
+    private int año, copiasDisponibles;
+
+
+public Libro(String titulo, String autor, String isbn, String genero, int año, int copiasDisponibles){
+    this.titulo=titulo;
+    this.autor=autor;
+    this.isbn=isbn;
+    this.genero=genero;
+    this.año=año;
+    this.copiasDisponibles=copiasDisponibles;
+}
 
 
 }
