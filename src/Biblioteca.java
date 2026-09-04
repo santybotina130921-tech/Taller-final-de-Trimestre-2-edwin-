6b8a1150499a97b01a757793c09b816d3f17a323
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-09-04 06:17:55 -0500

    metodo de guardar un libro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -10,6 +10,27 @@ public class Biblioteca {
         this.listaLibros = new ArrayList<>();
         this.mapaIsbn = new HashMap<>();
     }
-        public boolean agregarLibro(String titulo, String autor, String isbn, String genero, int año, int copias) {
-
+    public boolean agregarLibro(String titulo, String autor, String isbn, String genero, int año, int copias) {
+        if (titulo == null || titulo.trim().isEmpty() ||
+                autor == null || autor.trim().isEmpty() ||
+                isbn == null || isbn.trim().isEmpty() ||
+                genero == null || genero.trim().isEmpty()) {
+            return false;
         }
+        //isbn y ya esiste ellibro
+        if (mapaIsbn.containsKey(isbn)) {
+            return false;
+        }
+        if (año > 2026) {
+            return false;
+        }
+        if (copias < 0) {
+            return false;
+        }
+        Libro nuevoLibro = new Libro(titulo, autor, isbn, genero, año, copias);
+        listaLibros.add(nuevoLibro);
+        mapaIsbn.put(isbn, nuevoLibro);
+
+        return true; // Libro agregado con éxito
+    }}
+
