36c530cb466953be38821883b5101de61bfe3c37
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-09-04 07:26:37 -0500

    despues de consultar el HashSet y añadirlo quedo listo

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Biblioteca {
     private ArrayList<Libro> listaLibros;
     private HashMap<String, Libro> mapaIsbn;
+    private HashSet<String> codigosIsbn;
 
     // Constructor
     public Biblioteca() {
         this.listaLibros = new ArrayList<>();
         this.mapaIsbn = new HashMap<>();
+        this.codigosIsbn = new HashSet<>();
     }
 
     //agregar
@@ -21,7 +24,8 @@ public class Biblioteca {
             return false;
         }
         //isbn y ya esiste ellibro
-        if (mapaIsbn.containsKey(isbn)) {
+        // Usamos el HashSet para verificar  si ya existe el ISBN
+        if (codigosIsbn.contains(isbn)) {
             return false;
         }
         if (año > 2026) {
@@ -33,37 +37,39 @@ public class Biblioteca {
         Libro nuevoLibro = new Libro(titulo, autor, isbn, genero, año, copias);
         listaLibros.add(nuevoLibro);
         mapaIsbn.put(isbn, nuevoLibro);
+        codigosIsbn.add(isbn);
 
         return true;
     }
-        //obtener
-        public ArrayList<Libro> obtenerTodos() {
-            return listaLibros;
-        }
-        //buscar
-        public ArrayList<Libro> filtrarPorAutor(String autorBuscado) {
-            ArrayList<Libro> resultado = new ArrayList<>();
-            if (autorBuscado == null || autorBuscado.trim().isEmpty()) {
-                return resultado;
-            }
-
-            for (Libro libro : listaLibros) {
-                if (libro.getAutor().toLowerCase().contains(autorBuscado.toLowerCase().trim())) {
-                    resultado.add(libro);
-                }
-            }
+    //obtener
+    public ArrayList<Libro> obtenerTodos() {
+        return listaLibros;
+    }
+    //buscar
+    public ArrayList<Libro> filtrarPorAutor(String autorBuscado) {
+        ArrayList<Libro> resultado = new ArrayList<>();
+        if (autorBuscado == null || autorBuscado.trim().isEmpty()) {
             return resultado;
         }
 
-        //eliminar
-
-        public boolean eliminarLibro(String isbn) {
-            Libro libroAEliminar = mapaIsbn.get(isbn);
-            if (libroAEliminar != null) {
-                listaLibros.remove(libroAEliminar);
-                mapaIsbn.remove(isbn);
-                return true;
+        for (Libro libro : listaLibros) {
+            if (libro.getAutor().toLowerCase().contains(autorBuscado.toLowerCase().trim())) {
+                resultado.add(libro);
             }
-            return false;
         }
+        return resultado;
     }
+
+    //eliminar
+
+    public boolean eliminarLibro(String isbn) {
+        Libro libroAEliminar = mapaIsbn.get(isbn);
+        if (libroAEliminar != null) {
+            listaLibros.remove(libroAEliminar);
+            mapaIsbn.remove(isbn);
+            codigosIsbn.remove(isbn);
+            return true;
+        }
+        return false;
+    }
+}
