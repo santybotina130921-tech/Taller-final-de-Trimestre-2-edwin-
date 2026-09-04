e48e6b29731de1769a7a5f04b55e7d731d21e783
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-09-04 07:03:15 -0500

    metodos de boblioteca terminados

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -10,6 +10,9 @@ public class Biblioteca {
         this.listaLibros = new ArrayList<>();
         this.mapaIsbn = new HashMap<>();
     }
+
+    //agregar
+
     public boolean agregarLibro(String titulo, String autor, String isbn, String genero, int año, int copias) {
         if (titulo == null || titulo.trim().isEmpty() ||
                 autor == null || autor.trim().isEmpty() ||
@@ -31,6 +34,36 @@ public class Biblioteca {
         listaLibros.add(nuevoLibro);
         mapaIsbn.put(isbn, nuevoLibro);
 
-        return true; // Libro agregado con éxito
-    }}
+        return true;
+    }
+        //obtener
+        public ArrayList<Libro> obtenerTodos() {
+            return listaLibros;
+        }
+        //buscar
+        public ArrayList<Libro> filtrarPorAutor(String autorBuscado) {
+            ArrayList<Libro> resultado = new ArrayList<>();
+            if (autorBuscado == null || autorBuscado.trim().isEmpty()) {
+                return resultado;
+            }
 
+            for (Libro libro : listaLibros) {
+                if (libro.getAutor().toLowerCase().contains(autorBuscado.toLowerCase().trim())) {
+                    resultado.add(libro);
+                }
+            }
+            return resultado;
+        }
+
+        //eliminar
+
+        public boolean eliminarLibro(String isbn) {
+            Libro libroAEliminar = mapaIsbn.get(isbn);
+            if (libroAEliminar != null) {
+                listaLibros.remove(libroAEliminar);
+                mapaIsbn.remove(isbn);
+                return true;
+            }
+            return false;
+        }
+    }
